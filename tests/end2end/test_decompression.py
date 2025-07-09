a4fb3052f4277f5285873cf532bd25f27d5380cc
Olivia <oliviawolfie@pm.me>
2025-07-10 01:10:53 +0200

    Fuck browsers

--- tests/end2end/test_decompression.py
+++ tests/end2end/test_decompression.py
@@ -23,7 +23,9 @@ def _decompress(response):
 def test_decompress_with_nothing(driver: Chrome, httpbin: Httpbin):
     driver.get(f"{httpbin}/json")
     time.sleep(1)
-    res = driver.last_request.response
+    res = driver.requests[0].response
+    assert res.status_code == 200, res.body
+
     body = res.body
     assert body is not None
     assert len(body) > 0
@@ -37,27 +39,24 @@ def test_decompress_with_nothing(driver: Chrome, httpbin: Httpbin):
 
 def test_decompress_with_brotli(driver: Chrome, httpbin: Httpbin):
     driver.get(f"{httpbin}/brotli")
-    time.sleep(1)
-    res = driver.requests[-1].response
-    assert res.status_code == 200
+    res = driver.requests[0].response
+    assert res.status_code == 200, res.body
     assert res.headers.get("content-encoding") \
         == "br"
     _decompress(res)
 
 def test_decompress_with_deflate(driver: Chrome, httpbin: Httpbin):
     driver.get(f"{httpbin}/deflate")
-    time.sleep(1)
-    res = driver.requests[-1].response
-    assert res.status_code == 200
+    res = driver.requests[0].response
+    assert res.status_code == 200, res.body
     assert res.headers.get("content-encoding") \
         == "deflate"
     _decompress(res)
 
 def test_decompress_with_gzip(driver: Chrome, httpbin: Httpbin):
     driver.get(f"{httpbin}/gzip")
-    time.sleep(1)
-    res = driver.requests[-1].response
-    assert res.status_code == 200
+    res = driver.requests[0].response
+    assert res.status_code == 200, res.body
     assert res.headers.get("content-encoding") \
         == "gzip"
     _decompress(res)
